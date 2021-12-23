ea88f57071b2920cd009c1a719905ce2d84cbc4a
tsalesproductions <padilhauch@gmail.com>
2021-12-22 23:04:55 -0300

    buscando lista

--- components/abf.js
+++ components/abf.js
@@ -13,4 +13,63 @@ module.exports = class abfIntegration{
             data: params
         })
     }
+
+    /*======== INICIO DA CONFIGURAÇÃO DE LEAD ========*/
+    async createLead(req, lead){
+        const { data } = await this.query('POST', {
+            method: 'createLeads', 
+            params: {
+                objects: [
+                    lead
+                ],
+            },
+            id: req.id
+        });
+    
+        if(data.error.length > 0) return data.error;
+        if(data.result.creates.length > 0) return data.result.creates;
+    }
+
+    async getLead(req, {emailAddress}){
+        const { data } = await this.query('POST', {
+            method: 'getLeads', 
+            params: {
+                where: {emailAddress: emailAddress},
+                limit: 1
+            },
+            id: req.id
+        });
+
+        if(data.error) return data.error;
+        if(data.result.lead > 0) return data.result.lead[0];
+        return data.result.lead;
+    }
+
+    async checkLead(req, lead){
+        let response = await this.getLead(req, lead);
+
+        if(response.length <= 0){
+            return await this.createLead(req, lead);
+        }else{
+            return response[0]
+        }
+    }
+    /*======== FIM DA CONFIGURAÇÃO DE LEAD ========*/
+
+    /*======== INICIO DA CONFIGURAÇÃO DE LISTA ========*/
+    async getListMemberships(req, {emailAddress}){
+        const { data } = await this.query('POST', {
+            method: 'getListMemberships', 
+            params: {
+                emailAddress: emailAddress
+            },
+            id: req.id
+        });
+        
+        return [data];
+        // if(data.error) return data.error;
+        // if(data.result.lead > 0) return data.result.lead[0];
+        // return data.result.lead;
+    }
+    /*======== FIM DA CONFIGURAÇÃO DE LEAD ========*/
 }
--- modules/routes.js
+++ modules/routes.js
@@ -8,19 +8,12 @@ module.exports = class routes{
 
     routerList(){
         this.router.get('/', async function (req, res) {
-            const { data } = await abf.query('POST', {
-                method: 'getLeads', 
-                params: {
-                    where: {emailAddress: 'tobias.carvalho7@gmail.com'},
-                    limit: '10',
-                    offset: '0',
-                },
-                id: req.id
-            });
+            let lead = {firstName:"Thiagoo",lastName:"Saless",emailAddress:"tsalesssssssssssssss@yopmail.com"};
+
+            let data = await abf.getListMemberships(req, lead);
             
-            if(data.error) return res.json(data.error);
-            console.log(data)
-            return res.json(data.result)
+            res.json(data);
         })
     }
+    
 }
