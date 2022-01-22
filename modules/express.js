c72254a65f0b04bd4a8ead11f0be5e98339a8f59
tsalesproductions <thiago_salests@hotmail.com>
2022-01-21 21:09:09 -0300

    base update fix

--- modules/express.js
+++ modules/express.js
@@ -4,7 +4,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const requestID = require('express-request-id')
 
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
