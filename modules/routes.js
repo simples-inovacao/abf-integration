e3fe3ae0100688f3179b0d876ff15f60fd242edf
tsalesproductions <thiago_salests@hotmail.com>
2022-01-26 15:55:11 -0300

    oto fix

--- modules/routes.js
+++ modules/routes.js
@@ -10,7 +10,7 @@ module.exports = class routes{
         this.routerList();
     }
 
-    routerList(){
+    async routerList(){
         database = await abf.getClientsDatabase();
 
         this.router.get('/', async function (req, res) {
