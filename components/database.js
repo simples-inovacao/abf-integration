f9c26987b7cada7bd31a85e5b32a2dfe884fa88b
tsalesproductions <thiago_salests@hotmail.com>
2022-02-03 13:17:29 -0300

    fix?

--- components/database.js
+++ components/database.js
@@ -6,10 +6,10 @@ var fs = require('fs');
 module.exports = class database{
     getDatabase(name){
         try{
-            let database = new FileSync(`../database/${name}.json`);
-        let db = low(database);
-        db.defaults({ data: []}).write()
-        return db.get('data');
+            let database = new FileSync(`database/${name}.json`);
+            let db = low(database);
+            db.defaults({ data: []}).write()
+            // return db.get('data');
         }catch(e){
             console.log(e)
         }
