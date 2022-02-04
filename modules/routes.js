98b81eb7c49c36021f962250e56e88c0cdf1f05c
tsalesproductions <thiago_salests@hotmail.com>
2022-02-04 16:30:40 -0300

    sla

--- modules/routes.js
+++ modules/routes.js
@@ -82,9 +82,8 @@ module.exports = class routes{
         this.router.post('/automation/list/add', async function (req, res) {
             const { id, data } = req.body;
             console.log("OrderId:", `${id} ou ${data.id} ???`)
-            console.log(data)
             let c = cache.init();
-            await (await vtex.orders()).checkStatus(data.id, data.data, req, c);
+            await (await vtex.orders()).checkStatus(data.id, data, req, c);
 
             res.json({status: "ok"}) 
         })
